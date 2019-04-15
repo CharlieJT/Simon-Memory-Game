d9f31498e904d82a7c9507ea4071a65f1a787fad
Charlie Tipton <charliejt1992@hotmail.com>
2019-04-15 13:38:35 +0000

    Change class names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,26 @@
 let playerSeq = [];
 let compSeq = [];
 let lvl = 0;
+let greenSound = document.getElementById("green-audio"); //for audio
+let redSound = document.getElementById("red-audio");
+let yellowSound = document.getElementById("yellow-audio");
+let blueSound = document.getElementById("blue-audio");
+let gameOverSound = document.getElementById("game-over");
 
+
+// Initialising sequence.
 $(document).ready(function() {
-    $(".startBtn").on("click", function() {
+    $(".start-btn").on("click", function() {
         lvl++;
        startPlay(); 
     });
 });
 
+// Computer Sequence.
 function startPlay() {
-    $(".numDisplay").text(lvl);
+    $(".num-display").text(lvl);
+}
+
+function getRandomNumber() {
+    
 }
