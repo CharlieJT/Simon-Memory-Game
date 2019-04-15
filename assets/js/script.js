f45ff83a26dbd0c5369a189c04046fbf4f040a40
Charlie Tipton <charliejt1992@hotmail.com>
2019-04-15 17:52:17 +0000

    Add player sequence check & end of sequence to pad click function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 
 var playerSeq = [];
 var compSeq = [];
-var id, color, lvl, i = 0;
+var id, color, lvl = 0; // id, color & lvl are all initialised at 0
 var greenSound = document.getElementById("sound-green"); //for audio
 var redSound = document.getElementById("sound-red");
 var yellowSound = document.getElementById("sound-yellow");
@@ -16,29 +16,61 @@ $(document).ready(function() {
         lvl++;
         startPlay();
     });
+    
+    $(".pad").on("click", function() {
+        id = $(this).attr("id");
+        color = $(this).attr("class").split(" ")[1];
+        playerSeq.push(id);
+        console.log(id+" "+color);
+        addSoundClass(id, color);
+        // Checking person sequence
+        if (!personCorrect()) {
+            showError();
+            playerSeq = [];
+        }
+        // End of Sequence
+        if (playerSeq.length == compSeq.length) {
+            lvl++;
+            playerSeq = [];
+            startPlay();
+        }
+    })
 });
 
 // Computer Sequence.
 function startPlay() {
     console.log(lvl);
     $(".num-display").text(lvl);
-    getRandomNumber();
+    // getRandomNumber();
+    var i = 0;
     var myInterval = setInterval(function () {
         id = compSeq[i];
         color = $("#"+id).attr("class").split(" ")[1];
         console.log(id+" "+color)
         addSoundClass(id, color);
+        i++;
+        if (i == compSeq.length) {
+            i = 0;
+            clearInterval(myInterval);
+        }
     }, 1000);
 }
 
+// A Random number is generated
 function getRandomNumber() {
     var random = Math.floor(Math.random() * 4);
     compSeq.push(random);
 }
 
+// A Light & sound will be generated
 function addSoundClass(id, color) {
-    $("#"+id).addClass(color+"active");
+    $("#"+id).addClass(color+"-light");
+    soundGenerate();
     setTimeout(function() {
-      $("#"+id).removeClass(color+"active");  
+      $("#"+id).removeClass(color+"-light");  
     }, 500);
 }
+
+function soundGenerate() {
+    
+};
