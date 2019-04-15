96db28dd509a9be5b3ce7afd70a1acf587d7ca86
Charlie Tipton <charliejt1992@hotmail.com>
2019-04-15 15:16:43 +0000

    Add startPlay, getRandomNumber & addSoundClass functions to Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,44 @@
-let playerSeq = [];
-let compSeq = [];
-let lvl = 0;
-let greenSound = document.getElementById("green-audio"); //for audio
-let redSound = document.getElementById("red-audio");
-let yellowSound = document.getElementById("yellow-audio");
-let blueSound = document.getElementById("blue-audio");
-let gameOverSound = document.getElementById("game-over");
+// Variables
+
+var playerSeq = [];
+var compSeq = [];
+var id, color, lvl, i = 0;
+var greenSound = document.getElementById("sound-green"); //for audio
+var redSound = document.getElementById("sound-red");
+var yellowSound = document.getElementById("sound-yellow");
+var blueSound = document.getElementById("sound-blue");
+var gameOverSound = document.getElementById("sound-lost");
 
 
 // Initialising sequence.
 $(document).ready(function() {
     $(".start-btn").on("click", function() {
         lvl++;
-       startPlay(); 
+        startPlay();
     });
 });
 
 // Computer Sequence.
 function startPlay() {
+    console.log(lvl);
     $(".num-display").text(lvl);
+    getRandomNumber();
+    var myInterval = setInterval(function () {
+        id = compSeq[i];
+        color = $("#"+id).attr("class").split(" ")[1];
+        console.log(id+" "+color)
+        addSoundClass(id, color);
+    }, 1000);
 }
 
 function getRandomNumber() {
-    
+    var random = Math.floor(Math.random() * 4);
+    compSeq.push(random);
+}
+
+function addSoundClass(id, color) {
+    $("#"+id).addClass(color+"active");
+    setTimeout(function() {
+      $("#"+id).removeClass(color+"active");  
+    }, 500);
 }
