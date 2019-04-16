cacf911109d6ab8490149ef18e5066cc4e9f4113
Charlie Tipton <charliejt1992@hotmail.com>
2019-04-16 12:31:30 +0000

    Add sound files & implement sounds & on computer sequence & on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 
 var playerSeq = [];
 var compSeq = [];
+var active = false;
 var id, color, lvl = 0; // id, color & lvl are all initialised at 0
 var greenSound = document.getElementById("sound-green"); //for audio
 var redSound = document.getElementById("sound-red");
@@ -13,6 +14,9 @@ var gameOverSound = document.getElementById("sound-lost");
 // Initialising sequence.
 $(document).ready(function() {
     $(".start-btn").on("click", function() {
+        lvl = 0;
+        compSeq = [];
+        playerSeq = [];
         lvl++;
         startPlay();
     });
@@ -23,8 +27,8 @@ $(document).ready(function() {
         playerSeq.push(id);
         console.log(id+" "+color);
         addSoundClass(id, color);
-        // Checking person sequence
-        if (!personCorrect()) {
+        // Checking player sequence
+        if (!PersonSequenceCheck()) {
             showError();
             playerSeq = [];
         }
@@ -34,14 +38,41 @@ $(document).ready(function() {
             playerSeq = [];
             startPlay();
         }
-    })
+    });
 });
+
+function showError() {
+    console.log("Error!");
+    var count = 0;
+    var myError = setInterval(function() {
+        $(".num-display").text("Error");
+        
+        count++;
+        if (count == 3) {
+            $(".num-display").text(lvl);
+            clearInterval(myError);
+            playerSeq = [];
+            count = 0;
+            gameOverSound.play();
+        }
+    }, 200);
+};
+
+// This checks to ensure Player Sequence is the same as Computer Sequence
+function PersonSequenceCheck() {
+    for(var i = 0; i < playerSeq.length; i++) {
+        if (playerSeq[i] != compSeq[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 // Computer Sequence.
 function startPlay() {
     console.log(lvl);
     $(".num-display").text(lvl);
-    // getRandomNumber();
+    getRandomNumber();
     var i = 0;
     var myInterval = setInterval(function () {
         id = compSeq[i];
@@ -53,7 +84,7 @@ function startPlay() {
             i = 0;
             clearInterval(myInterval);
         }
-    }, 1000);
+    }, 800);
 }
 
 // A Random number is generated
@@ -65,12 +96,12 @@ function getRandomNumber() {
 // A Light & sound will be generated
 function addSoundClass(id, color) {
     $("#"+id).addClass(color+"-light");
-    soundGenerate();
+    blueSoundGenerate();
     setTimeout(function() {
       $("#"+id).removeClass(color+"-light");  
     }, 500);
 }
 
-function soundGenerate() {
-    
+function blueSoundGenerate() {
+    blueSound.play();
 };
