f15bd11366fc6615d4a0997cf4fc22c3e5265121
Charlie Tipton <charliejt1992@hotmail.com>
2019-04-25 18:53:47 +0000

    Add variables to select each clickable pad or button

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,15 @@ let compTurn;
 let intervalId;
 let strictMode = false;
 let sound = true;
+
+// Clickable buttons & pads targetted as variables
+const greenPad = document.getElementById("0");
+const redPad = document.getElementById("1");
+const yellowPad = document.getElementById("2");
+const bluePad = document.getElementById("3");
+const startButton = document.getElementById("start");
+const strictButton = document.getElementById("strict")
+
+strictButton.addEventListener('click', (event) => {
+        console.log("changed");
+});
