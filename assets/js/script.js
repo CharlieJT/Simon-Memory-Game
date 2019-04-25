07061601b39b1cc9ac681bc39432a66faa9af086
Charlie Tipton <charliejt1992@hotmail.com>
2019-04-25 21:47:23 +0000

    Add Default play setting

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,9 @@ let intervalId;
 let strictMode = false;
 let sound = true;
 
-// Clickable buttons & pads targetted as variables
+// buttons, number display & pads targetted as variables using jQuery
+
+const numDisplay = document.getElementById("number-counter");
 const greenPad = document.getElementById("0");
 const redPad = document.getElementById("1");
 const yellowPad = document.getElementById("2");
@@ -18,7 +20,7 @@ const bluePad = document.getElementById("3");
 const startButton = document.getElementById("start");
 const strictButton = document.getElementById("strict")
 
-// Event Listener checks to see if strict slider is true or false
+// Event Listener checks to see if strict slider is true or false when clicked
 strictButton.addEventListener('click', (event) => {
     if (strictButton.checked == true) {
         strict = true;
@@ -26,3 +28,23 @@ strictButton.addEventListener('click', (event) => {
         strict = false;
     }
 });
+
+// Event Listener initialisation when start button is clicked
+startButton.addEventListener('click', (event) => {
+    play();
+});
+
+// Default play setting
+function play() {
+    sequence = [];
+    playerSequence = [];
+    light = 0;
+    intervalId = 0;
+    turn = 1;
+    numDisplay.innerHTML = "1";
+    correct = true;
+    for (var i = 0; i < turn; i++) {
+        sequence.push(Math.floor(Math.random()* 4) +1);
+    }
+    compTurn = true;
+}
