b4aca7646c86335bf0efd8ccd03dd69183d48b85
Charlie Tipton <charliejt1992@hotmail.com>
2019-05-03 14:11:37 +0000

    Change file names & test initial values in play function with Jasmine

--- assets/jasmine-testing/spec/simonSpec.js
+++ assets/jasmine-testing/spec/simonSpec.js
@@ -1,11 +1,31 @@
 describe('play function', function() {
 
     beforeEach(() => {
+        light = 0;
+        turn = 1;
         correct = true;
+        compTurn = true;
+    });
+
+    it('should return light = 0', function() {
+        let result = play();
+        expect(light).toBe(0);
+    });
+
+    it('should return turn = 1', function() {
+        let result = play();
+        expect(turn).toBe(1);
     });
 
     it('should return correct = true', function() {
         let result = play();
         expect(correct).toBe(true);
     });
+
+    it('should return compTurn = true', function() {
+        let result = play();
+        expect(compTurn).toBe(true);
+    });
 });
+
+
