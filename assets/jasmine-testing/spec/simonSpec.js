de7804f4f288d768ccb48285f0a8260ce74d50aa
Charlie Tipton <charliejt1992@hotmail.com>
2019-05-04 15:30:13 +0000

    Change each test to describe each test at a time

--- assets/jasmine-testing/spec/simonSpec.js
+++ assets/jasmine-testing/spec/simonSpec.js
@@ -7,26 +7,35 @@ describe('play function', function() {
         compTurn = true;
     });
 
-    it('should return light = 0', function() {
-        let result = play();
-        expect(light).toBe(0);
+    describe('Value of light variable', function() {
+        it('should return light = 0', function() {
+            let result = play();
+            expect(light).toBe(0);
+        });
     });
-
-    it('should return turn = 1', function() {
-        let result = play();
-        expect(turn).toBe(1);
+    
+    describe('Value of turn variable', function() {
+        it('should return turn = 1', function() {
+            let result = play();
+            expect(turn).toBe(1);
+        });
     });
-
-    it('should return correct = true', function() {
-        let result = play();
-        expect(correct).toBe(true);
+    
+    describe('Value of correct variable', function() {
+        it('should return correct = true', function() {
+            let result = play();
+            expect(correct).toBe(true);
+        });
     });
-
-    it('should return compTurn = true', function() {
-        let result = play();
-        expect(compTurn).toBe(true);
+    
+    describe('Value of compTurn variable', function() {
+       it('should return compTurn = true', function() {
+            let result = play();
+            expect(compTurn).toBe(true);
+        }); 
     });
-
+});    
+/*
     describe('classes of pads', function() {
 
         beforeEach(() => {
@@ -35,53 +44,62 @@ describe('play function', function() {
 			<div id="1" class="pad red-pad disabled"></div>
 			<div id="2" class="pad yellow-pad disabled"></div>
 			<div id="3" class="pad blue-pad disabled"></div>
-        `)
+			<div id="number-counter" class="num-display">-</div>
+            `);
         });
-        
+
+        describe('display on number display', function() {
+            it('should return number counter to equal -', function() {
+                let result = play();
+                expect($("#number-counter").text()).toEqual("-");
+            });
+        });
+
         describe('disabled pads', function() {
 
             it('should return green-pad as having class disabled', function() {
-                play();
+                let result = play();
                 expect('.green-pad').toHaveClass('disabled');
             });
 
             it('should return red-pad as having class disabled', function() {
-                play();
+                let result = play();
                 expect('.red-pad').toHaveClass('disabled');
             });
 
             it('should return yellow-pad as having class disabled', function() {
-                play();
+                let result = play();
                 expect('.yellow-pad').toHaveClass('disabled');
             });
 
             it('should return blue-pad as having class disabled', function() {
-                play();
+                let result = play();
                 expect('.blue-pad').toHaveClass('disabled');
             });
         });
-        
-        describe('green pads', function() {
+
+        describe('light on pads', function() {
 
             it('should return green-pad as not having class green-light', function() {
-                play();
+                let result = play();
                 expect('.green-pad').not.toHaveClass('green-light');
             });
 
             it('should return red-pad as not having class red-light', function() {
-                play();
+                let result = play();
                 expect('.red-pad').not.toHaveClass('red-light');
             });
 
             it('should return yellow-pad as not having class yellow-light', function() {
-                play();
+                let result = play();
                 expect('.yellow-pad').not.toHaveClass('yellow-light');
             });
 
             it('should return blue-pad as not having class blue-light', function() {
-                play();
+                let result = play();
                 expect('.blue-pad').not.toHaveClass('blue-light');
             });
         });
     });
 });
+*/
