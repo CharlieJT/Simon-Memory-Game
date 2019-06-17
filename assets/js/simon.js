a0561728c6d3f7d8fc9787b5faeae40e1bef8374
Charlie Tipton <charliejt1992@hotmail.com>
2019-06-17 19:46:18 +0000

    Add light class to each of the pads

--- assets/js/simon.js
+++ assets/js/simon.js
@@ -9,6 +9,7 @@ let sound = true;
 // Buttons, number displays, pads & modals targetted as variables using JQuery.
 
 const numDisplay = document.getElementById("number-counter");
+const pad = document.getElementsByClassName('pad');
 const greenPad = document.getElementById("0");
 const redPad = document.getElementById("1");
 const yellowPad = document.getElementById("2");
@@ -22,3 +23,44 @@ const winModalDisplay = document.getElementById("win-modal-display");
 
 // Whatever code is written inside the JQuery ready method will run once the page Document Object Modal (DOM) is ready to execute JavaScript code.
 
+$('.pad').click(function() {
+    
+    if ($(this).attr('id') == 0) {
+        green();
+    }
+    if ($(this).attr('id') == 1) {
+        red();
+    }
+    if ($(this).attr('id') == 2) {
+        yellow();
+    }
+    if ($(this).attr('id') == 3) {
+        blue();
+    }
+});
+
+function green() {
+    $(greenPad).addClass('green-light')
+    setTimeout(function() {
+        $(greenPad).removeClass('green-light')
+    }, 300);
+}
+function red() {
+    $(redPad).addClass('red-light')
+    setTimeout(function() {
+        $(redPad).removeClass('red-light')
+    }, 300);
+}
+function yellow() {
+    $(yellowPad).addClass('yellow-light')
+    setTimeout(function() {
+        $(yellowPad).removeClass('yellow-light')
+    }, 300);
+}
+
+function blue() {
+    $(bluePad).addClass('blue-light')
+    setTimeout(function() {
+        $(bluePad).removeClass('blue-light')
+    }, 300);
+}
