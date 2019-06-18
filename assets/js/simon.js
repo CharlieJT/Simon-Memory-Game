bc80a7deb166f510424b722ac76fbffe2217f98e
Charlie Tipton <charliejt1992@hotmail.com>
2019-06-18 18:02:39 +0000

    Add feature to push number into sequence and player sequence arrays and display in console

--- assets/js/simon.js
+++ assets/js/simon.js
@@ -2,7 +2,7 @@
 
 let sequence = [];
 let playerSequence = [];
-let light, turn, correct, compTurn, intervalId, win;
+let light, turn, correct, compTurn, win;
 let strictMode = true;
 let sound = true;
 
@@ -10,10 +10,10 @@ let sound = true;
 
 const numDisplay = document.getElementById("number-counter");
 const pad = document.getElementsByClassName('pad');
-const greenPad = document.getElementById("0");
-const redPad = document.getElementById("1");
-const yellowPad = document.getElementById("2");
-const bluePad = document.getElementById("3");
+const greenPad = document.getElementById("1");
+const redPad = document.getElementById("2");
+const yellowPad = document.getElementById("3");
+const bluePad = document.getElementById("4");
 const startButton = document.getElementById("start");
 const strictButton = document.getElementById("strict");
 const startModal = document.getElementById("start-modal-button");
@@ -30,49 +30,62 @@ $(document).ready(function() {
 
     $('.pad').click(function() {
         let padId = $(this).attr('id');
-        if (padId == 0) {
-            greenLightAndSound();
-        }
         if (padId == 1) {
-            redLightAndSound();
+            greenLightAndSound();
+            sequence.push[padId];
+            console.log(padId)
         }
         if (padId == 2) {
-            yellowLightAndSound();
+            redLightAndSound();
+            sequence.push[padId];
+            console.log(padId)
         }
         if (padId == 3) {
+            yellowLightAndSound();
+            sequence.push[padId];
+            console.log(padId)
+        }
+        if (padId == 4) {
             blueLightAndSound();
+            sequence.push[padId];
+            console.log(padId)
         }
     });
 });
 
 function startPlay() {
     randomNumber();
+    sequence = [];
+    $(numDisplay).text('0');
+    playerSequence, sequence = [];
+    turn = 0;
 }
 
 function randomNumber() {
-    randomNum = Math.floor(Math.random() * 4)
+    randomNum = Math.ceil(Math.random() * 4);
     setTimeout(function() {
-        if (randomNum == 0) {
+        if (randomNum == 1) {
             greenLightAndSound();
         }
-        if (randomNum == 1) {
+        if (randomNum == 2) {
             redLightAndSound();
         }
-        if (randomNum == 2) {
+        if (randomNum == 3) {
             yellowLightAndSound();
         }
-        if (randomNum == 3) {
+        if (randomNum == 4) {
             blueLightAndSound();
         }
-    console.log(randomNum)
     }, 500);
+    sequence.push(randomNum)
+    console.log(sequence);
 }
 
 function greenLightAndSound() {
     $(greenPad).addClass('green-light')
     setTimeout(function() {
         $(greenPad).removeClass('green-light')
-    }, 300);
+    }, 400);
     let soundGreen = $('#sound-green')[0];
     soundGreen.currentTime = 0;
     soundGreen.play();
@@ -82,7 +95,7 @@ function redLightAndSound() {
     $(redPad).addClass('red-light')
     setTimeout(function() {
         $(redPad).removeClass('red-light')
-    }, 300);
+    }, 400);
     let soundRed = $('#sound-red')[0];
     soundRed.currentTime = 0;
     soundRed.play();
@@ -92,7 +105,7 @@ function yellowLightAndSound() {
     $(yellowPad).addClass('yellow-light')
     setTimeout(function() {
         $(yellowPad).removeClass('yellow-light')
-    }, 300);
+    }, 400);
     let soundYellow = $('#sound-yellow')[0];
     soundYellow.currentTime = 0;
     soundYellow.play();
@@ -102,8 +115,15 @@ function blueLightAndSound() {
     $(bluePad).addClass('blue-light')
     setTimeout(function() {
         $(bluePad).removeClass('blue-light')
-    }, 300);
+    }, 400);
     let soundBlue = $('#sound-blue')[0];
     soundBlue.currentTime = 0;
     soundBlue.play();
 }
+
+function check() {
+    if (sequence && playerSequence == turn) {
+        
+    }
+}
+
